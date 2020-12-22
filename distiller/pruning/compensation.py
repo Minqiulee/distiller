f095d4bd0f13b7a3696d9997a71f6a53701f35f7
Minqiu <minqiulee@gmail.com>
2020-12-22 21:13:18 +0800

    202012222113

--- distiller/pruning/compensation.py
+++ distiller/pruning/compensation.py
@@ -1,10 +1,10 @@
 import distiller
+from .similarity import *
 
 class CompensatePrunner(object):
-    def __init__(self, name, sensitivities, criteria, threshold, bn_lambda, **kwargs):
+    def __init__(self, name, sensitivities, threshold, bn_lambda, **kwargs): # choose a pruning criterion.(default: sensitivity pruning) Or, otherwise, should try to implement similarity merging method in the feture.
         self.name = name
         self.sensitivities = sensitivities
-        self.criteria = criteria # choose which criterion to use at first masking step
         self.threshold = threshold
         self.bn_lambda = bn_lambda # parameter used after batch normalization layer
 
@@ -19,6 +19,9 @@ class CompensatePrunner(object):
 
         # first mask the tensor based on sensitivity criterion.
         zeros_mask_dict[param_name].mask = distiller.create_mask_sensitivity_criterion(param, sensitivity)
+
+        # import main algo ./similiarity.py
+        merge_pruning_compensation(model=)
 
         # compensate masked elements with elements kept.
         zeros_mask_dict[param_name].mask = distiller.create_mask_similarity_criterion(param, self.threshold, self.bn_lambda)
